56262edac11c236904a088d8e751bf3635693752
Cyrill Püntener <wp99cp@gmx.net>
2019-12-17 18:20:03 +0100

    added week overview to export

--- src/app/application/_pages/export-camp/export-camp.component.ts
+++ src/app/application/_pages/export-camp/export-camp.component.ts
@@ -7,6 +7,7 @@ import { Camp } from '../../_class/camp';
 import { MatAccordion } from '@angular/material/expansion';
 import { ActivatedRoute } from '@angular/router';
 import { map, flatMap } from 'rxjs/operators';
+import { firestore } from 'firebase';
 
 /** ExportCampComponent:
  * Export Seite für Lager. Möglichkeit ein in eMeal erstelltes Lager als
@@ -48,8 +49,9 @@ export class ExportCampComponent implements OnInit {
   public today: string;
   public user: Observable<User>;
   public shoppingListWithError: Observable<any>;
-  protected campInfo: Observable<any>;
-  protected mealsInfo: Observable<any>;
+  public campInfo: Observable<any>;
+  public weekTable: Observable<any>;
+  public mealsInfo: Observable<any>;
 
   constructor(private route: ActivatedRoute, private authService: AuthenticationService, private databaseService: DatabaseService) {
 
@@ -67,11 +69,70 @@ export class ExportCampComponent implements OnInit {
 
     this.shoppingListWithError = this.campId.pipe(flatMap(campId => this.databaseService.getShoppingList(campId)));
     this.campInfo = this.campId.pipe(flatMap(campId => this.databaseService.getCampInfoExport(campId)));
+
+    this.weekTable = this.campInfo.pipe(map(campInfo => {
+
+      const days = campInfo.data.days;
+
+      // sortieren nach Datum
+      days.sort((a, b) => a.date - b.date);
+
+      const tableHeaders = [];
+
+      interface HashTable {
+        [key: string]: string[];
+      }
+      const rows: HashTable = {};
+
+      days.forEach((day: { date: any; meals: [{ title: string; description: string; }] }) => {
+
+        tableHeaders.push(new Date(day.date._seconds * 1000).toDateString());
+
+        // sort meals
+        day.meals.sort((a, b) => a.title.localeCompare(b.title));
+
+        // add meals of day
+        day.meals.forEach(meal => {
+          if (rows[meal.title] === undefined) {
+            rows[meal.title] = [];
+
+            for (let i = 0; i < tableHeaders.length - 1; i++) {
+              rows[meal.title].push('-');
+            }
+
+          }
+          rows[meal.title].push(meal.description);
+        });
+
+        // add empty
+        for (const key in rows) {
+          if (rows[key].length < tableHeaders.length) {
+            rows[key].push('-');
+          }
+        }
+
+      });
+
+      const newRows = [];
+      const rowTitles = [];
+
+      for (const key in rows) {
+
+        newRows.push(rows[key]);
+        rowTitles.push(key);
+
+      }
+
+      const result: any = { tableHeaders, rowEntries: newRows, rowTitles };
+      return result;
+
+    }));
     this.mealsInfo = this.databaseService.getMealsInfoExport();
 
+    // print on console
     this.shoppingListWithError.subscribe(console.log);
     this.campInfo.subscribe(console.log);
-
+    this.weekTable.subscribe(console.log);
     this.mealsInfo.subscribe(console.log);
 
   }
