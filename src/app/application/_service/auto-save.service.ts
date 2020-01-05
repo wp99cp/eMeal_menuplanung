e01c3dfd5611e44483f6db11a6f18282a024dae9
Cyrill Püntener <wp99cp@gmx.net>
2020-01-05 17:15:05 +0100

    Update auto-save.service.ts
    - add comments

--- src/app/application/_service/auto-save.service.ts
+++ src/app/application/_service/auto-save.service.ts
@@ -1,10 +1,15 @@
 import { Injectable, NgZone } from '@angular/core';
-import { CanDeactivate } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { CanDeactivate } from '@angular/router';
 
 
 export interface Saveable {
 
+  /**
+   * Saves all unsaved changes in this Component.
+   *
+   * @returns should return true if some changes got saved and false otherwise
+   */
   save: () => Promise<boolean>;
 
 }
@@ -14,29 +19,36 @@ export interface Saveable {
   providedIn: 'root'
 })
 /**
- * AutoSaveService
+ * AutoSaveService: Handelt das automatisierte Speichern beim Wechseln einer Seite.
+ *
+ *  Aktiviertung:
+ *  Muss im module-rooting aktiviert werden und der entsprechende Component muss das Interface Saveable implemntieren
  */
 export class AutoSaveService implements CanDeactivate<Saveable> {
 
-  constructor(
-    public snackBar: MatSnackBar,
-    private zone: NgZone
-  ) { }
+  constructor(public snackBar: MatSnackBar, private zone: NgZone) { }
 
+  /**
+   * Wird beim Wechsel der Seiten aufgerufen.
+   */
   canDeactivate(component: Saveable) {
 
+    component.save()
+      .then(saved => {
 
-
-    component.save().then(
-      saved => {
+        // Benachrichtigung, falls Änderungen gespeichert wurden.
         if (saved) {
           this.zone.run(() => {
+
+            // Anzeige des Benachrichtigungs-Banners
             this.snackBar.open('Änderungen wurden automatisch gespeichert!', '', { duration: 2000 });
+
           });
         }
-      }
-    );
 
+      });
+
+    // Erlaubt das Wechseln der Seite(return true).
     return true;
 
   }
