1ecc9f751a8e188b9ca9766d068c6873b5b05016
Cyrill Püntener <wp99cp@gmx.net>
2020-01-30 08:30:55 +0100

    layout changes

--- src/app/application/_dialoges/share-dialog/share-dialog.component.ts
+++ src/app/application/_dialoges/share-dialog/share-dialog.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { Camp } from '../../_class/camp';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material';
-import { DatabaseService } from '../../_service/database.service';
-import { AuthenticationService } from '../../_service/authentication.service';
 import { map } from 'rxjs/operators';
+
+import { Camp } from '../../_class/camp';
 import { Meal } from '../../_class/meal';
 import { Recipe } from '../../_class/recipe';
+import { AccessData } from '../../_interfaces/accessData';
+import { User } from '../../_interfaces/user';
+import { AuthenticationService } from '../../_service/authentication.service';
+import { DatabaseService } from '../../_service/database.service';
 
 @Component({
   selector: 'app-share-dialog',
@@ -28,31 +31,49 @@ export class ShareDialogComponent implements OnInit {
   ngOnInit() {
   }
 
+  jsonConcat(o1, o2) {
+    for (const key in o2) {
+      o1[key] = o2[key];
+    }
+    return o1;
+  }
+
   /** A user get selected */
   selectUser(selectedCoworkers) {
 
 
-    this.auth.getCurrentUser().pipe(map(user => {
-      const accessData = Camp.generateCoworkersList(user.uid, selectedCoworkers);
-      accessData[user.uid] = 'owner';
-      return accessData;
-    })).subscribe(access => {
+    this.auth.getCurrentUser()
+      .pipe(this.createCoworkerList(selectedCoworkers))
+      .subscribe((access: AccessData) => {
 
-      this.camp.access = access;
+        // merge the old access data with the new one
+        access = this.jsonConcat(access, this.camp.access);
 
-      this.databaseService.updateAccessData(access, Camp.getPath(this.camp.firestoreElementId));
-      this.camp.days.forEach(day => day.meals.forEach(meal => {
-        this.databaseService.updateAccessData(access, Meal.getPath(meal.firestoreElementId));
+        this.camp.access = access;
 
-        this.databaseService.getRecipes(meal.firestoreElementId).subscribe(recipes =>
-          recipes.forEach(recipe =>
-            this.databaseService.updateAccessData(access, Recipe.getPath(meal.firestoreElementId, recipe.firestoreElementId))
-          ));
+        this.databaseService.updateAccessData(access, Camp.getPath(this.camp.firestoreElementId));
+        this.camp.days.forEach(day => day.meals.forEach(meal => {
+          this.databaseService.updateAccessData(access, Meal.getPath(meal.firestoreElementId));
 
-      }));
+          this.databaseService.getRecipes(meal.firestoreElementId).subscribe(recipes =>
+            recipes.forEach(recipe =>
+              this.databaseService.updateAccessData(access, Recipe.getPath(meal.firestoreElementId, recipe.firestoreElementId))
+            ));
 
-    });
+        }));
+
+      });
 
   }
 
+
+  private createCoworkerList(selectedCoworkers: any) {
+
+    return map((user: User) => {
+      const accessData = Camp.generateCoworkersList(user.uid, selectedCoworkers);
+      accessData[user.uid] = 'owner';
+      return accessData;
+    });
+
+  }
 }
--- src/app/application/_template/list-of-users/list-of-users.component.ts
+++ src/app/application/_template/list-of-users/list-of-users.component.ts
@@ -3,7 +3,7 @@ import { AccessData } from '../../_interfaces/accessData';
 import { Observable } from 'rxjs';
 import { DatabaseService } from '../../_service/database.service';
 import { User } from '../../_interfaces/user';
-import { mergeMap } from 'rxjs/operators';
+import { mergeMap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-list-of-users',
@@ -22,7 +22,9 @@ export class ListOfUsersComponent implements OnChanges {
 
     if (this.userList != null) {
 
-      this.owners = this.dbService.getUsers(this.userList);
+      // creates an alphabeticaly sorted list of the users
+      this.owners = this.dbService.getUsers(this.userList)
+        .pipe(map(results => results.sort((a, b) => a.displayName.localeCompare(b.displayName))));
 
     }
 
