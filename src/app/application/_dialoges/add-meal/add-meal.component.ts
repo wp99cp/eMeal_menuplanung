ab9d51feb9b79322552f81ca5902ff6fe973bfce
Cyrill Püntener <wp99cp@gmx.net>
2020-01-30 08:30:33 +0100

    bug fix in paginator

--- src/app/application/_dialoges/add-meal/add-meal.component.ts
+++ src/app/application/_dialoges/add-meal/add-meal.component.ts
@@ -15,11 +15,11 @@ import { ImportComponent } from '../import/import.component';
 
 export function CustomPaginator() {
   const customPaginatorIntl = new MatPaginatorIntl();
-  customPaginatorIntl.itemsPerPageLabel = 'Mahlzeiten pro Zeite';
+  customPaginatorIntl.itemsPerPageLabel = 'Mahlzeiten pro Seite';
   customPaginatorIntl.getRangeLabel = ((page: number, pageSize: number, length: number) => {
 
     length = Math.max(length, 0);
-    const startIndex = (page * pageSize === 0 && length !== 0) ? 1 : page * pageSize;
+    const startIndex = (page * pageSize === 0 && length !== 0) ? 1 : (page * pageSize + 1);
     // If the start index exceeds the list length, do not try and fix the end index to the end.
     const endIndex = Math.min(startIndex - 1 + pageSize, length);
     return startIndex + ' bis ' + endIndex + ' von ' + length;
@@ -28,7 +28,6 @@ export function CustomPaginator() {
   return customPaginatorIntl;
 }
 
-// TODO: fix bug, deselect nach der Auswahl der Verwendung...
 @Component({
   selector: 'app-add-meal',
   templateUrl: './add-meal.component.html',
--- src/app/application/_pages/camp-list-page/camp-list-page.component.ts
+++ src/app/application/_pages/camp-list-page/camp-list-page.component.ts
@@ -23,10 +23,10 @@ export function CustomPaginator() {
   customPaginatorIntl.getRangeLabel = ((page: number, pageSize: number, length: number) => {
 
     length = Math.max(length, 0);
-    const startIndex = (page * pageSize === 0 && length !== 0) ? 1 : page * pageSize;
+    const startIndex = (page * pageSize === 0 && length !== 0) ? 1 : (page * pageSize + 1);
     // If the start index exceeds the list length, do not try and fix the end index to the end.
     const endIndex = Math.min(startIndex - 1 + pageSize, length);
-    return startIndex + " bis " + endIndex + " von " + length
+    return startIndex + ' bis ' + endIndex + ' von ' + length;
   });
 
   return customPaginatorIntl;
