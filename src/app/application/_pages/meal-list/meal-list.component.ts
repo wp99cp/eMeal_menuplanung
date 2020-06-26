c57055cd67dfa84fc2e1cb099fa7148df81a9d36
Cyrill Püntener <34008738+wp99cp@users.noreply.github.com>
2020-06-26 13:14:22 +0200

    changes to the imports

--- src/app/application/_pages/meal-list/meal-list.component.ts
+++ src/app/application/_pages/meal-list/meal-list.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { take, mergeMap } from 'rxjs/operators';
-import { Meal } from '../../_class/meal';
-import { DeepCopyMealComponent } from '../../_dialoges/deep-copy-meal/deep-copy-meal.component';
-import { DatabaseService } from '../../_service/database.service';
-import { TileListPage } from '../tile_page';
-import { CreateMealComponent } from '../../_dialoges/create-meal/create-meal.component';
-import { Observable } from 'rxjs';
-import { FirestoreMeal } from '../../_interfaces/firestoreDatatypes';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { MatDialog } from '@angular/material/dialog';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {mergeMap, take} from 'rxjs/operators';
+import {Meal} from '../../_class/meal';
+import {DeepCopyMealComponent} from '../../_dialoges/deep-copy-meal/deep-copy-meal.component';
+import {DatabaseService} from '../../_service/database.service';
+import {TileListPage} from '../tile_page';
+import {CreateMealComponent} from '../../_dialoges/create-meal/create-meal.component';
+import {Observable} from 'rxjs';
+import {FirestoreMeal} from '../../_interfaces/firestoreDatatypes';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {MatDialog} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-meal-list',
@@ -23,64 +23,39 @@ export class MealListComponent extends TileListPage<Meal> implements OnInit {
     super(dbService, snackBar, dbService.getEditableMeals(), dialog);
 
     // set filter for searching
-    this.filterFn = (meal) => meal.name.toLocaleLowerCase().includes(this.filterValue.toLocaleLowerCase())
+    this.filterFn = (meal) => meal.name.toLocaleLowerCase().includes(this.filterValue.toLocaleLowerCase());
     this.dbElementName = 'Mahlzeit';
-
-  }
-
-  protected newElement() {
-
-    this.dialog.open(CreateMealComponent, {
-      height: '640px',
-      width: '900px',
-      data: { mealName: '' }
-    }).afterClosed()
-      .pipe(mergeMap((meal: Observable<FirestoreMeal>) => meal))
-      .subscribe(mealData => this.dbService.addDocument(mealData, 'meals'));
 
   }
 
   ngOnInit() {
 
-
     this.addButtonNew();
 
-    this.route.queryParams.subscribe(params => {
+    // TODO: better version
+    setTimeout(() =>
+      this.route.queryParams.subscribe(params => {
 
-      const usesParameter = params['includes'];
+        const usesParameter = params.includes;
 
-      if (usesParameter) {
-        (document.getElementById('search-field') as HTMLFormElement).value = 'includes: ' + usesParameter;
-        this.applyFilter('includes: ' + usesParameter);
+        if (usesParameter) {
 
-      }
+          (document.getElementById('search-field') as HTMLFormElement).value = 'includes: ' + usesParameter;
+          this.applyFilter('includes: ' + usesParameter);
+          console.log(params);
 
-    });
+        }
 
-
-  }
-
-  protected async deleteConditions(element: Meal): Promise<boolean> {
-
-    return new Promise(resolve => this.dbService.getNumberOfUses(element.documentId).subscribe(numb => {
-
-      if (numb != 0)
-        this.snackBar.open('Das Rezept kann nicht gelöscht werden, da es noch verwendet wird!', '', { duration: 2000 });
-
-      resolve(numb == 0);
-
-    }));
+      }), 250);
 
   }
+
 
   applyFilter(event: string) {
 
-    // TODO: dieser Filter geht nich nicht, muss in der abstrac class implementiert werden, 
-    // da das Lager diese Funktion ebenfalls benötigt...
-
     if (event.includes('includes:')) {
 
-      let recipeId = event.substr(event.indexOf(':') + 1).trim();
+      const recipeId = event.substr(event.indexOf(':') + 1).trim();
       this.dbService.getMealsThatIncludes(recipeId).pipe(take(1))
         .subscribe(meals => this.filteredElements = meals);
 
@@ -121,6 +96,31 @@ export class MealListComponent extends TileListPage<Meal> implements OnInit {
 
         }
       });
+
+  }
+
+  protected newElement() {
+
+    this.dialog.open(CreateMealComponent, {
+      height: '640px',
+      width: '900px',
+      data: {mealName: ''}
+    }).afterClosed()
+      .pipe(mergeMap((meal: Observable<FirestoreMeal>) => meal))
+      .subscribe(mealData => this.dbService.addDocument(mealData, 'meals'));
+
+  }
+
+  protected async deleteConditions(element: Meal): Promise<boolean> {
+
+    return new Promise(resolve => this.dbService.getNumberOfUses(element.documentId).subscribe(numb => {
+
+      if (numb != 0)
+        this.snackBar.open('Das Rezept kann nicht gelöscht werden, da es noch verwendet wird!', '', {duration: 2000});
+
+      resolve(numb == 0);
+
+    }));
 
   }
 
