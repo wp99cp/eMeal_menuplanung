da3f1aa4694e94fef885cb4205d51b299270b29c
Cyrill Püntener <34008738+wp99cp@users.noreply.github.com>
2021-05-01 17:10:06 +0200

    Update footer and add link to lagerkueche.ch

--- frontend/src/app/_template/template-footer/template-footer.component.ts
+++ frontend/src/app/_template/template-footer/template-footer.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { version } from '../../../../package.json';
-import { copyrights } from '../../../../package.json';
+import {Component, OnInit} from '@angular/core';
+import {copyrights} from '../../../../package.json';
 import buildInfo from '../../../build';
+import {SwissDateAdapter} from '../../utils/format-datapicker';
 
 @Component({
   selector: 'app-template-footer',
@@ -13,7 +13,13 @@ export class TemplateFooterComponent implements OnInit {
   public copyrights: string = copyrights;
   public buildInfo = buildInfo;
 
-  constructor() { }
+  public buildDate;
+
+  constructor(public swissDateAdapter: SwissDateAdapter) {
+
+    this.buildDate = swissDateAdapter.format(new Date(buildInfo.timestamp));
+
+  }
 
   ngOnInit() {
   }
