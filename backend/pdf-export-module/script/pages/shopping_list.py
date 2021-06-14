bf17204cfcf2f6d235be44b36ce88e84eff55fbc
Cyrill Püntener <34008738+wp99cp@users.noreply.github.com>
2021-06-14 15:46:53 +0200

    show correct shopping list in pdf

--- backend/pdf-export-module/script/pages/shopping_list.py
+++ backend/pdf-export-module/script/pages/shopping_list.py
@@ -1,9 +1,11 @@
 from argparse import Namespace
 
-from pylatex import Section, Command, Package, Description, Subsubsection, Document
+from pylatex import Section, Command, Package, Description, Document, NoEscape
+from pylatex.utils import bold
 
 from exportData.camp import Camp
 from pages.enviroments import Multicols
+from shopping_list.shopping_list import ShoppingList
 
 
 def add_shopping_list(doc: Document, camp: Camp, args: Namespace):
@@ -12,24 +14,29 @@ def add_shopping_list(doc: Document, camp: Camp, args: Namespace):
 
     # space between colums
     doc.append(Command('setlength'))
-    doc.append(Command('columnsep', arguments='40pt'))
+    doc.append(Command('columnsep', arguments='25pt'))
 
     doc.packages.add(Package('multicol'))
     doc.packages.add(Package('enumitem'))
     doc.packages.add(Package('setspace'))
 
-    for _ in range(1):
-        doc.append(Subsubsection('Gemüse und Früchte', numbering=False))
+    shoppingList = ShoppingList(camp)
+    shoppingList.create_full_shopping_list()
+    full_shopping_list = shoppingList.full_shopping_list
 
-        with doc.create(Multicols(arguments='2')) as multicols:
+    for category_name in full_shopping_list.keys():
+        doc.append(bold(category_name))
+
+        with doc.create(Multicols(arguments='3')) as multicols:
             multicols.append(Command('small'))
 
-            with multicols.create(Description(options='leftmargin=1.75cm, itemsep=4pt')) as itemize:
+            with multicols.create(Description(options='leftmargin=0.5cm, itemsep=4pt')) as itemize:
                 # space between colums
                 itemize.append(Command('setlength', arguments=Command('itemsep'), extra_arguments='0pt'))
                 itemize.append(Command('setlength', arguments=Command('parskip'), extra_arguments='0pt'))
 
-                itemize.add_item('100g', 'the first item')
-                itemize.add_item('23 Stk.', 'Bananen')
-                itemize.add_item('100g', 'the first item')
-                itemize.add_item('10g', 'the item')
+                for ing in full_shopping_list[category_name]:
+                    itemize.add_item('[ ]', ing['food'] + ', ' +
+                                     (str(ing['measure_calc']) if ing['measure_calc'] > 0 else ' ') + ' ' + ing['unit'])
+
+        doc.append(NoEscape(r'\vspace{0.5cm} \noindent'))
