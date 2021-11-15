d257ef3712e86ff4d1312ce3dabf0baa81c7201b
Cyrill Püntener <wp99cp@gmx.net>
2021-11-15 15:38:51 +0100

    add time stamp to PDF creation date

--- frontend/src/app/utils/format-datapicker.ts
+++ frontend/src/app/utils/format-datapicker.ts
@@ -15,6 +15,15 @@ export class SwissDateAdapter extends NativeDateAdapter {
       { year: 'numeric', month: 'short', day: '2-digit' });
   }
 
+
+  /**
+   * Creates String out of date with the form "30. Jan. 2020"
+   */
+  public format_time(date: Date): string {
+    return date.toLocaleDateString('de-CH',
+      { year: 'numeric', month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit' });
+  }
+
   /**
    * Creates String out of date with the form "Montag, 03. Feb. 2020"
    */
