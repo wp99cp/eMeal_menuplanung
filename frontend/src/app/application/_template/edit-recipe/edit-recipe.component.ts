b0ecd2a5f044552b2b9537d7e85dedee9873c06c
Cyrill Püntener <wp99cp@gmx.net>
2021-12-14 08:34:05 +0100

    fix bug: fresh column should not be selectable

--- frontend/src/app/application/_template/edit-recipe/edit-recipe.component.ts
+++ frontend/src/app/application/_template/edit-recipe/edit-recipe.component.ts
@@ -162,6 +162,11 @@ export class EditRecipeComponent implements OnInit {
     setTimeout(() => {
       let str = '';
       target.classList.forEach(classStr => str += '.' + classStr);
+
+      // No focus on fresh column
+      if(str.includes("fresh-column"))
+        return;
+
       this.setFocus(target.parentElement.parentElement.previousElementSibling.firstElementChild.querySelector(str));
     }, 20);
 
