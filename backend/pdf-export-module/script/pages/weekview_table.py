wp99cp/eMeal_menuplanung

commit 71d7b576f86e7d2897658a5dd7500255d73d6285

fix bug: wrong prepare date

backend/pdf-export-module/script/pages/weekview_table.py
@@ -49,6 +49,7 @@ def prepareMealsForWeekview(camp: Camp, args: Namespace):
                 meal_weekview.get('Vorbereiten')[day_index] += \
                     NoEscape(meal.get('meal_weekview_name') + r" \par \vspace{0.1cm} {\tiny \textit{für " +
                              (meal.get('meal_prepare_date') + timedelta(hours=2)).strftime("%A") +
+                             (meal.get('meal_date') + timedelta(hours=2)).strftime("%A") +
                              r'}} \vspace{0.20cm}  \par ')
 
     return meal_weekview

commit 4fa74862c16dd322695a15bdbb34b0d68da017b3

fix bug introduced in commit 71d7b576

backend/pdf-export-module/script/pages/weekview_table.py
@@ -49,7 +49,9 @@ def prepareMealsForWeekview(camp: Camp, args: Namespace):
             if prepare_date in day_as_dates:
                 day_index = day_as_dates.index(prepare_date)
                 meal_weekview.get('Vorbereiten')[day_index] += \
-                             (meal.get('meal_date') + timedelta(hours=2)).strftime("%A") + r'}} \vspace{0.20cm}  \par '
+                    NoEscape(meal.get('meal_weekview_name') + r" \par \vspace{0.1cm} {\tiny \textit{für " +
+                             (meal.get('meal_date') + timedelta(hours=2)).strftime("%A") +
+                             r'}} \vspace{0.20cm}  \par ')
 
     return meal_weekview
 