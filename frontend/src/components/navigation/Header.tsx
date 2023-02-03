9ba5d2268f15f9412ea717d5bd123209af198173
Cyrill Püntener <wp99cp@gmx.net>
2023-02-03 13:15:11 +0100

    add basic tour component for help messages

--- frontend/src/components/navigation/Header.tsx
+++ frontend/src/components/navigation/Header.tsx
@@ -25,7 +25,7 @@ function MobileNavigation() {
     >
       <Popover.Panel
         focus
-        className="absolute inset-x-0 top-0 z-50 origin-top-right transform p-2 transition md:hidden"
+        className="absolute inset-x-0 top-0 z-30 origin-top-right transform p-2 transition md:hidden"
       >
         <div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
           <div className="px-5 pt-5 pb-6">
