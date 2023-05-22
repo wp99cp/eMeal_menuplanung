4cf9a66d5b2e60725051fbb38ec8ea1d8fcf4172
Cyrill Püntener <wp99cp@gmx.net>
2023-05-22 21:05:58 +0200

    clean up docker setup, db is now persistent across container restarts

--- backend/graphQL/entrypoint.sh
+++ backend/graphQL/entrypoint.sh
@@ -1,17 +1,17 @@
 echo "Wait for MongoDB to be ready..."
+# shellcheck disable=SC2188
 until </dev/tcp/mongo-db/27018; do sleep 1; done
+
 # Prepare the database
 export PRISMA_OUTPUT="../../backend/graphQL/src/util/generated/prisma/client/"
 dotenv -e "$ENV_FILE_PATH" -- npx prisma generate --schema=../../common/prisma/schema.prisma
 dotenv -e "$ENV_FILE_PATH" -- npx prisma db push --schema=../../common/prisma/schema.prisma
 
-# Migrate and seed the database
-dotenv -e "$ENV_FILE_PATH" -- npx prisma migrate dev --schema=../../common/prisma/schema.prisma
+# Seed the database
 dotenv -e "$ENV_FILE_PATH" -- npx prisma db seed
 
 # Start Prisma Studio in the background
 dotenv -e "$ENV_FILE_PATH" -- npx prisma studio --schema=../../common/prisma/schema.prisma &
-
 # Generate the GraphQL schema types
 dotenv -e "$ENV_FILE_PATH" -- npm run generate
 # Start the GraphQL server
--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,19 +1,16 @@
 echo "Wait for MongoDB to be ready..."
+# shellcheck disable=SC2188
 until </dev/tcp/mongo-db/27018; do sleep 1; done
 
-# Prepare the database
+# Generates the Prisma client for the frontend
+# We use Prisma for NextAuth, all other operations are done via the backend graphql server
 export PRISMA_OUTPUT="../../frontend/src/util/generated/prisma/client/"
 dotenv -e "$ENV_FILE_PATH" -- npx prisma generate --schema=../common/prisma/schema.prisma
-dotenv -e "$ENV_FILE_PATH" -- npx prisma db push --schema=../common/prisma/schema.prisma
 
 # Generate the GraphQL schema types
 dotenv -e "$ENV_FILE_PATH" -- npm run generate
 
-# Start the NextJS in dev mode
-# dotenv -e "$ENV_FILE_PATH" -- npm run lint
-
 # Create build.ts file with git information
 sed -i 's/\\r//g' create_build_info.sh
 source ./create_build_info.sh
-
 dotenv -e "$ENV_FILE_PATH" -- npm run dev
