94adaa26f8b126a21c72366bdfbc5b4bb4f8d8d3
Cyrill Püntener <wp99cp@gmx.net>
2024-05-18 11:53:27 +0200

    fix wrapper function for tracing
    before the error was not propagated correctly

--- backend/graphQL/src/tracing/traceWrapper.ts
+++ backend/graphQL/src/tracing/traceWrapper.ts
@@ -1,12 +1,13 @@
-import { trace } from '@opentelemetry/api';
+import { Exception, trace } from '@opentelemetry/api';
 
 const tracer = trace.getTracer('graphQL-backend');
 
 /**
  *
- * A generic, typed wrapper for tracing functions using OpenTelemetry.
+ * A generic and typed wrapper for tracing functions using OpenTelemetry.
  *
- * @param fn - The function to be traced.
+ *
+ * @param fn - The function to be traced (can be synchronous or asynchronous).
  * @param fnName - The name of the function to be traced.
  *
  * @returns The wrapped function result.
@@ -16,11 +17,50 @@ export const traceWrapper =
   <T extends unknown[], U>(fn: (..._: T) => U, fnName?: string): ((...__: T) => U) =>
   (...args: T): U =>
     tracer.startActiveSpan(fnName || fn.name || 'traceWrapper', (span) => {
-      const result = fn(...args);
+      let result;
 
-      // if U is of type Promise, end the span when the promise resolves
-      if (result instanceof Promise) result.then(() => span.end());
-      else span.end(); // else end the span immediately
+      try {
+        result = fn(...args);
+
+        // if the result is a promise, we need to catch any
+        // exceptions and record them before ending the span
+        if (result instanceof Promise)
+          result
+            .catch((e) => {
+              span.recordException(e);
+              span.setAttribute('error', true);
+            })
+            .finally(() => span.end());
+      } catch (e) {
+        span.recordException(e as Exception);
+        span.setAttribute('error', true);
+        throw e;
+      } finally {
+        if (!(result instanceof Promise)) span.end();
+      }
 
       return result;
     });
+
+/**
+ *
+ * A decorator to wrap a method with the traceWrapper function.
+ *
+ * Example:
+ *
+ * ```typescript
+ * class MyClass {
+ *   @traceWrapperDecorator()
+ *   myMethod() {
+ *     // do something
+ *   }
+ * }
+ * ```
+ *
+ *
+ */
+export const traceWrapperDecorator =
+  () =>
+  (_: never, propertyKey: string, descriptor: PropertyDescriptor): void => {
+    descriptor.value = traceWrapper(descriptor.value, propertyKey);
+  };
